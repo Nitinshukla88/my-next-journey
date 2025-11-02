ccc0690598851666a3c3743cc89d522e52bfd9da
nitinshukla <nitin.shukla12004@gmail.com>
2025-11-02 17:37:02 +0530

    hitted the post api call to server

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,12 +3,21 @@
 // Since Next.js is an opinionated framework, this is a generally enforced opinion by Next.js to keep the code organized and maintainable.
 
 import { NextRequest } from "next/server";
+// After migrating the database to Prisma, we can use Prisma client to interact with the database.
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest){ // for headers and parameters we can use Request object as function parameter.
     // This method can be of any type like GET, POST, DELETE, PUT, PATCH etc. For that we'll replace above GET with POST, DELETE, PUT, PATCH etc.
 
     // make the database call and check if user is present and if yes then log the user and send the message.
     const body = await req.json();
+    // await prisma.user.create({
+    //     data : {
+    //         username : body.email,
+    //         password : "hashed_password" // In real application, password should be hashed before storing in database.
+    //     }
+    // });
     console.log(body);
     return Response.json({
         message : "you are logged in"
